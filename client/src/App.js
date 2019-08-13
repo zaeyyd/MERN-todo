d8a60f8ba130e7cd788d31eee878670bc29145c5
zaeyyd <zayyed.not@gmail.com>
2019-08-13 16:52:16 -0400

    This better workkkk

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppNavBar from './components/AppNavBar';
+import ShoppingList from './components/ShoppingList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavBar/>
+      <ShoppingList/>
+    </div>
+  );
+}
+
+export default App;
